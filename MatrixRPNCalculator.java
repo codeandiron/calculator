d46f65ec0034f4b3b7d0f6406165dfeec603ef4d
Matt Holtom <mholtom@admeris.com>
2012-04-14 11:07:47 -0400

    Adding initial rough cuts of MatrixRPNCalculator and the invoking sh script.

---
+++ MatrixRPNCalculator.java
@@ -0,0 +1,17 @@
+class MatrixRPNCalculator {
+	public static void main(String[] args) {
+		//Input of two file names
+		//Read that file in
+		//Split it on CRLF and commas into two D array of Strings
+			//Or put it in a HashMap of 
+			//A1->5 6 +
+			//B2->A1
+			//B3->8
+		//Start with A1? pass into something that decides what it is?
+		//3 types are simple, cell referencing, operating
+			//if it's a cell reference, value.process, recursive?, key.process, return
+			//if it's an operation, key.process, return
+			//if it's a simple number, return
+		//Once you have the result set, build up the output and dump it.
+	}
+}
