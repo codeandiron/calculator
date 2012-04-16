37b592407fa3dc4d9a3a40a4e0407463731ad843
Matt Holtom <mholtom@admeris.com>
2012-04-15 22:55:50 -0400

    Modified IOUtils to get the CSV file input from an absolute path. Hopefully this will make it easier for the evaluator.

--- src/main/java/com/kontagent/IOUtils.java
+++ src/main/java/com/kontagent/IOUtils.java
@@ -1,12 +1,22 @@
 package com.kontagent;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
 public final class IOUtils {
 	
 	public static String getInput(String fileName){		
-		InputStream fileInput = IOUtils.class.getResourceAsStream("../../" + fileName);
+		InputStream fileInput = null;
+		try {
+		fileInput = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not find your specified CSV file. " +
+					"Please make sure it is absolute and you have read permissions on that file.");
+			e.printStackTrace();
+			System.exit(0);
+		}
 		return new Scanner(fileInput).useDelimiter("\\A").next();
 	}
 	
